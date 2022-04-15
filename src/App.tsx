fea35508ff3dd7186327c865e5879b3054518358
Kivorob <71591773+Kivorob@users.noreply.github.com>
2022-04-16 09:39:25 +1000

    add scrolling menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import "./app.css"
 import EnterLine from "./components/EnterLine/EnterLine";
 import HeaderInner from "./components/HeaderInner/HeaderInner";
+import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
+import ScrollMenu from "./components/ScrollMenu/ScrollMenu";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
         <div className="app">
-          <EnterLine/>
+            <EnterLine/>
             <HeaderInner/>
+            <NavigationMenu/>
+            <ScrollMenu/>
+            <Footer/>
         </div>
     );
 };
---
+++ src/components/ScrollMenu/ScrollMenu.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {useState, useRef, useEffect} from "react";
+import debounce from "lodash.debounce"
+import classNames from "classnames"
+import styles from "./scrollmenu.module.css";
+import Add1 from "../../img/Add1.jpg";
+import Add2 from "../../img/Add2.jpeg";
+import Add3 from "../../img/Add3.jpg";
+
+export const ScrollMenu = () => {
+    const [canScrollLeft, setCanScrollLeft] = useState<boolean>(false);
+    const [canScrollRight, setCanScrollRight] = useState<boolean>(false);
+
+    const listRef = useRef<HTMLUListElement>(null);
+
+    const checkForScrollPosition = () => {
+        const {current} = listRef;
+        if (current) {
+            const {scrollLeft, scrollWidth, clientWidth} = current;
+            setCanScrollLeft(scrollLeft > 0);
+            setCanScrollRight(scrollLeft !== scrollWidth - clientWidth);
+        }
+    };
+
+    const debounceCheckForScrollPosition = debounce(checkForScrollPosition, 200);
+
+    const scrollContainerBy = (distance: number) =>
+        listRef.current?.scrollBy({left: distance, behavior: "smooth"});
+
+    useEffect(() => {
+        const {current} = listRef;
+        checkForScrollPosition();
+        current?.addEventListener("scroll", debounceCheckForScrollPosition);
+
+        return () => {
+            current?.removeEventListener("scroll", debounceCheckForScrollPosition);
+            debounceCheckForScrollPosition.cancel();
+        };
+    }, []);
+
+    return (
+        <div className={styles._bg}>
+            <div className={styles.container}>
+                <div className={styles.scrmenu}>
+                    <button
+                        type="button"
+                        disabled={!canScrollLeft}
+                        onClick={() => scrollContainerBy(-1210)}
+                        className={classNames("button", "buttonLeft", {
+                            "button--hidden": !canScrollLeft
+                        })}
+                    >
+                        ←
+                    </button>
+                    <ul className={styles.list} ref={listRef}>
+                        <li className="item"><img src={Add1} alt="1 картинка"/></li>
+                        <li className="item"><img src={Add2} alt="2 картинка"/></li>
+                        <li className="item"><img src={Add3} alt="3 картинка"/></li>
+                    </ul>
+                    <button
+                        type="button"
+                        disabled={!canScrollRight}
+                        onClick={() => scrollContainerBy(1210)}
+                        className={classNames("button", "buttonRight", {
+                            "button--hidden": !canScrollRight
+                        })}
+                    >
+                        →
+                    </button>
+                    {canScrollLeft ? (
+                        <div className="shadowWrapper leftShadowWrapper">
+                            <div className="shadow leftShadow" />
+                        </div>
+                    ) : null}
+                    {canScrollRight ? (
+                        <div className="shadowWrapper rightShadowWrapper">
+                            <div className="shadow rightShadow" />
+                        </div>
+                    ) : null}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ScrollMenu;
