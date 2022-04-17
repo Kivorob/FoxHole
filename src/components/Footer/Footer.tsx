5ad79012b817b8db4d9435c86c00482e9bbc952d
LirioNOne <dagguri.vs@students.dvfu.ru>
2022-04-17 12:46:14 +1000

    added footer and changed some files

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,20 +4,82 @@ import styles from "./footer.module.css"
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <div>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-            </div>
-            <div>
+            <div className={styles.container}>
+                <div className={styles.footer__column}>
+                    <div className={styles.footer__row}>
+                        <div className={styles.footer__block}>
+                            <div className={styles.footer__block_title}>
+                                Помощь
+                            </div>
+                            <ul className={styles.footer__navList}>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Доставка и оплата
+                                    </a>
+                                </li>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Возврат и обмен заказа
+                                    </a>
+                                </li>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Договор оферты
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.footer__block}>
+                            <div className={styles.footer__block_title}>
+                                Лисья Нора
+                            </div>
+                            <ul className={styles.footer__navList}>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        О компании
+                                    </a>
+                                </li>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Оптовым клиентам
+                                    </a>
+                                </li>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Контакты
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.footer__block}>
+                            <div className={styles.footer__block_title}>
+                                Блог
+                            </div>
+                            <ul className={styles.footer__navList}>
+                                <li className={styles.footer__navList_item}>
+                                    <a href="#" className={styles.footer__item_link}>
+                                        Статьи
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
 
-            </div>
-            <div>
+                </div>
+                <div className={styles.footer__block}>
+                    <div className={styles.footer__block_title}>
+                        Наши соцсети
+                    </div>
+                    <div className={styles.footer__icons}>
+                    </div>
+                    <div className={styles.footer__connection}>
+                        8 423 266-14-14
+                    </div>
 
+                    {/*<div className={styles.footer__info}></div>*/}
+                </div>
             </div>
-            <div>
 
-            </div>
         </div>
     );
 };
